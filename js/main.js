c8c766811063da780c32ecfd689e56531a73be20
admin <admin@admins-MacBook-Air.local>
2024-06-07 10:01:48 -0700

    added dark mode

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btnDarkModo = document.querySelector(".dark-mode-btn");
+
+btnDarkModo.onclick = function () {
+  btnDarkModo.classList.toggle("dark-mode-btn-active");
+};
+
+console.log(document.getElementById("btn-dark"));
